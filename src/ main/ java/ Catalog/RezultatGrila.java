afc14a54e98eef8b3ea2f1dd8b1a8b3c9902af98
IuliaElena1 <44519746+IuliaElena1@users.noreply.github.com>
2019-03-31 11:17:40 +0300

    Update and rename src/main/java/Catalog/RezultatGrila.java to  src/ main/ java/ Catalog/RezultatGrila.java
    .

--- src/ main/ java/ Catalog/RezultatGrila.java
+++ src/ main/ java/ Catalog/RezultatGrila.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2019 SSI Schaefer Noell GmbH
- *
- * $Header: /data/cvs/Scolarizare/csilistru/isi_test9/src/com/ssn/ssijs/isi/Catalog/RezultatGrila.java,v 1.1 2019/02/28 08:56:43 csilistru Exp $
- */
 
 package Catalog;
 
